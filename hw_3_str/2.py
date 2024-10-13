91a06c2c529e03849cd9a0c20c534ff157f52434
tenshi <zakan.6000@gmail.com>
2024-10-13 11:52:49 +0800

    hw 3 str done

--- hw_3_str/2.py
+++ hw_3_str/2.py
@@ -3,10 +3,9 @@ s = input("Введите строку: ")
 words = set()
 
 for i in range(n):
-    s_temp = s[i:-1]
-    for j in range(0, n, 3):
-        word = s_temp[i:i+3]
-        if len(word) == 3:
-            words.add(s_temp[i:i+3])
+    temp = [s[i:][j:j+m] for j in range(0, n, m)]
+    for word in temp:
+       if len(word) == m:
+           words.add(word) 
     n -= 1
-print(words)
+print(len(words), words)
