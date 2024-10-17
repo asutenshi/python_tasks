8ad912065d63bdec7923f13ae6b232ffd2619c3f
TenshiLenovo <zakan.6000@gmail.com>
2024-10-17 10:06:41 +0800

    add auxiliary functions

--- hw_3_str/2.py
+++ hw_3_str/2.py
@@ -1,11 +1,20 @@
+def get_substrings(s, n, m):
+    substrings = []
+    for i in range(n):
+        temp = [s[i:][j:j+m] for j in range(0, n, m)]
+        substrings.extend(temp)
+        n -= 1
+    return substrings
+
 n, m = map(int, input("Введите m и n: ").split())
 s = input("Введите строку: ")
 words = set()
 
-for i in range(n):
-    temp = [s[i:][j:j+m] for j in range(0, n, m)]
-    for word in temp:
-       if len(word) == m:
-           words.add(word) 
-    n -= 1
-print(len(words), words)
+# Вызов вспомогательной функции
+substrings = get_substrings(s, n, m)
+
+for word in substrings:
+    if len(word) == m:
+        words.add(word)
+
+print(len(words))
